0c6bb22c71fb08b7ebc25f331796f9d74e4cdbe9
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-07-07 19:01:52 +0200

    add task 6

--- 06-data-types/finish.js
+++ 06-data-types/finish.js
@@ -12,3 +12,22 @@
  * 2. Выведите в консоль тип каждого из значений
  * используя оператор typeof
  */
+
+const stringVar = "Hello!";
+const numVar = 100;
+const bolVar = true;
+const nullVar = null;
+const undefVar = undefined;
+const objVar = {
+  name: "Katia",
+  surname: "Zhyrnova",
+};
+const arrVar = [1, 2, 3];
+
+console.log(`The type of variable 'stringVar' is ${typeof stringVar}`);
+console.log(`The type of variable 'numVar' is ${typeof numVar}`);
+console.log(`The type of variable 'bolVar' is ${typeof bolVar}`);
+console.log(`The type of variable 'nullVar' is ${typeof nullVar}`);
+console.log(`The type of variable 'undefVar' is ${typeof undefVar}`);
+console.log(`The type of variable 'objVar' is ${typeof objVar}`);
+console.log(`The type of variable 'arrVar' is ${typeof arrVar}`);
