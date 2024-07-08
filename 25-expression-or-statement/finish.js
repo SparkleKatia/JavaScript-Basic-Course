e97f2843fdb6805e7b8fcdbb243ba7ada9069caf
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-07-08 10:53:49 +0200

    add task 25

--- 25-expression-or-statement/finish.js
+++ 25-expression-or-statement/finish.js
@@ -6,23 +6,24 @@
  *  - выражение-инструкция (expression statement)
  */
 
-15
+15; // выражение (expression)
 
 const myObject = {
+  // выражение (expression)
   x: 10,
   y: true,
-}
+}; // инструкция (statement)
 
-myObject.z = 'abc'
+myObject.z = "abc"; //выражение-инструкция (expression statement)
 
-delete myObject.x
+delete myObject.x; // выражение-инструкция (expression statement)
 
-let newVariable
+let newVariable; // инструкция (statement)
 
-newVariable = 30 + 5
+newVariable = 30 + 5; // выражение-инструкция (expression statement)
 
-console.log(newVariable)
+console.log(newVariable); //выражение-инструкция (expression statement)
 
 if (newVariable > 10) {
-  console.log(`${newVariable} больше 10`)
-}
+  console.log(`${newVariable} больше 10`);
+} // инструкция (statement)
