564ba121520c0bdfca6ffe7c2dfb5c4c53b4006d
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-07-14 14:21:50 +0200

    add task 61

--- 61-sum-numbers/finish.js
+++ 61-sum-numbers/finish.js
@@ -8,11 +8,19 @@
  * 3. Также верните эту сумму как результат функции
  */
 
-sumNumbers(1, 3)
+function sumNumbers() {
+  let sum = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+console.log(sumNumbers(1, 3));
 // 4
 
-sumNumbers(10, 20, 5)
+sumNumbers(10, 20, 5);
 // 35
 
-sumNumbers(2, 5, 80, 1, 10, 12)
+sumNumbers(2, 5, 80, 1, 10, 12);
 // 110
