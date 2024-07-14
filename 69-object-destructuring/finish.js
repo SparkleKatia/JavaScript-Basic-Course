e1617935940f94e2fac3e1e4fa5b1e7a9d0bc26e
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-07-14 15:03:28 +0200

    add task 70

--- 69-object-destructuring/finish.js
+++ 69-object-destructuring/finish.js
@@ -10,10 +10,11 @@ const objectWithNumbers = {
   x: 5,
   y: 20,
   z: 3,
-}
+};
 
 // Создайте функцию здесь
+const mult = ({ x, y, z }) => x * y * z;
 
-const result = mult(objectWithNumbers)
-console.log(result)
+const result = mult(objectWithNumbers);
+console.log(result);
 // 300
