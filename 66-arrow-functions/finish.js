c2d357993c149ed723e5a22fbe97c0e53eeb8f9e
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-07-14 14:48:50 +0200

    add task 65

--- 66-arrow-functions/finish.js
+++ 66-arrow-functions/finish.js
@@ -3,11 +3,9 @@
  * Замените обычные функции на стрелочные
  */
 
-function mult(a, b) {
-  return a * b
-}
+mult = (a, b) => a * b;
 
-setTimeout(function () {
-  console.log(mult(5, 10))
-}, 1000)
+setTimeout(() => {
+  console.log(mult(5, 10));
+}, 1000);
 // 50
