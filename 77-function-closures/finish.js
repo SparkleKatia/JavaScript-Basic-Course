521fc601da78451682f18e2b3e807652001e5717
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2024-07-14 15:51:30 +0200

    add task 77

--- 77-function-closures/finish.js
+++ 77-function-closures/finish.js
@@ -14,19 +14,35 @@
  *  - changeGreeting
  */
 
-const greeting1 = createGreeting()
+const createGreeting = () => {
+  let greetingString = "Hey, this is";
 
-console.log(greeting1.greet('Bob'))
+  function greet(name) {
+    return `${greetingString} ${name}`;
+  }
+
+  function changeGreeting(newGreeting) {
+    greetingString = newGreeting;
+  }
+
+  return {
+    greet,
+    changeGreeting,
+  };
+};
+const greeting1 = createGreeting();
+
+console.log(greeting1.greet("Bob"));
 // Hey, this is Bob
 
-greeting1.changeGreeting('Good Morning from')
+greeting1.changeGreeting("Good Morning from");
 
-console.log(greeting1.greet('Emily'))
+console.log(greeting1.greet("Emily"));
 // Good Morning from Emily
 
 /* ____________  */
 
-const greeting2 = createGreeting()
+const greeting2 = createGreeting();
 
-console.log(greeting2.greet('Emily'))
+console.log(greeting2.greet("Emily"));
 // Hey, this is Emily
